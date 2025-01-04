300e5d80827f579360674f17e3afbe5021a2a70e
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-01-05 01:42:36 +0700

    Add constants and enums for user management and JWT configuration
    - Created a new file `collections.ts` to define constants for user and token collections.
    - Updated `config.ts` to include additional JWT configuration variables for access and refresh tokens, as well as email verification tokens.
    - Introduced a new file `enums.ts` to define enums for user verification statuses and token types, enhancing type safety and clarity in the codebase.
    
    These changes improve the organization of constants and configurations related to user management and JWT handling, facilitating easier maintenance and readability.

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -14,4 +14,13 @@ export const envConfig = {
     dbPassword: process.env.DB_PASSWORD as string,
     dbName: process.env.DB_NAME as string,
     port: (process.env.PORT as string) || 8000,
+
+    // jwt
+    jwtSecretAccessToken: process.env.JWT_SECRET_ACCESS_TOKEN as string,
+    jwtSecretRefreshToken: process.env.JWT_SECRET_REFRESH_TOKEN as string,
+    accessTokenExpiresIn: process.env.JWT_EXPIRES_IN_ACCESS_TOKEN as string,
+    refreshTokenExpiresIn: process.env.JWT_EXPIRES_IN_REFRESH_TOKEN as string,
+    jwtSecretEmailVerifyToken: process.env.JWT_SECRET_EMAIL_VERIFY_TOKEN as string,
+    emailVerifyTokenExpiresIn: process.env.JWT_EXPIRES_IN_EMAIL_VERIFY_TOKEN as string,
+
 } as const
