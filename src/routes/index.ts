c7536400195d97d40b3288afa50d12072eb70ce5
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-01-05 01:47:39 +0700

    Enhance routing by adding access routes and modifying root endpoint
    - Updated the root router to include a new endpoint at "/helpers" that responds with a welcome message.
    - Integrated access routes by importing and using `accessRouters`, facilitating user authentication and access management.
    
    These changes improve the routing structure of the application, supporting future user authentication features.

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import { Router } from 'express'
 import usersRouters from './users.routes'
+import accessRouters from './access.routes';
 
 
 const rootRouterV1 = Router()
-rootRouterV1.get("/", (req, res) => {
+rootRouterV1.get("/helpers", (req, res) => {
     console.log("Hello World")
     res.status(200).send({ message: "Welcome to Express & TypeScript Server" });
 });
 rootRouterV1.use('/user', usersRouters)
+rootRouterV1.use('/access', accessRouters)
 
 export default rootRouterV1
