4c9ceadfde373f655e8b5a6c23fb28c72419a623
kennynguyen1603 <kennynguyen1631984@gmail.com>
2025-01-07 18:59:36 +0700

    Update Express Request interface to include user and decoded_authorization properties

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,8 +1,8 @@
 declare global {
     namespace Express {
         interface Request {
-            // user?: UserDocument
-            // decoded_authorization?: TokenPayload
+            user?: IUser
+            decoded_authorization?: TokenPayload
             // decoded_refresh_token?: TokenPayload
             // decoded_email_verify_token?: TokenPayload
             // decoded_forgot_password_token?: TokenPayload
